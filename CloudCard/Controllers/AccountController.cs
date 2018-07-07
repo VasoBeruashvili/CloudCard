ed3cd203845e9380b977ffe2f6146a99baa7b0d8
vasoberuashvili <vasil.beruashvili@gmail.com>
2018-07-07 16:43:30 +0400

    Add project files.

---
+++ CloudCard/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using CloudCard.Models;
+
+namespace CloudCard.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {        
+        //
+        // GET: /Account/Login
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
